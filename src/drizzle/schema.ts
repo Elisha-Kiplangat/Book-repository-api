2f91b59e2afc8fab643c5f9d1d7e0b737d6894e7
Elisha-Kiplangat <elishakiplanga@gmail.com>
2024-06-26 14:39:30 +0300

    hono

---
+++ src/drizzle/schema.ts
@@ -0,0 +1,9 @@
+import { pgTable, text, varchar, serial, date, integer } from 'drizzle-orm/pg-core';
+
+
+export const bookTable = pgTable("book", {
+    id: serial("id").primaryKey(),
+    title: varchar("title", { length: 255 }).notNull(),
+    author: varchar("author", { length: 255 }).notNull(),
+    publication_year: integer("publication_year").notNull(),
+});
